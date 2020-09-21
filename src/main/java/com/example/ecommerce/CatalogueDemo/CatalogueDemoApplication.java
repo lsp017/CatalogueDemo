a7c84309acd75057e7983e992a5f3fb73a79a372
lsp017 <lsp017.mit@gmail.com>
2020-09-21 15:44:27 +0530

    Initial commit

---
+++ src/main/java/com/example/ecommerce/CatalogueDemo/CatalogueDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce.CatalogueDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogueDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogueDemoApplication.class, args);
+	}
+
+}
